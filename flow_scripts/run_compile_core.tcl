168107180c06737f542062351be6d07971fb9fea
Marco Kurzynski <marcokurzynski@icloud.com>
2025-04-22 23:49:21 -0400

    part 1

--- flow_scripts/run_compile_core.tcl
+++ flow_scripts/run_compile_core.tcl
@@ -4,26 +4,48 @@ set outDir "./results"
 define_design_lib work -path ./WORK
 
 ####### Synopsys 14nm #######
-set target_library "/home/net/local/SAED14nm_EDK_08_2024/SAED14nm_EDK_STD_RVT/liberty/nldm/base/saed14rvt_base_ss0p72v125c.db"
-set link_library "/home/net/local/SAED14nm_EDK_08_2024/SAED14nm_EDK_STD_RVT/liberty/nldm/base/saed14rvt_base_ss0p72v125c.db" 
+# ran on huskey.eecs.ucf.edu
+set target_library "/home1/local/SAED14nm_EDK_08_2024/SAED14nm_EDK_STD_RVT/liberty/nldm/base/saed14rvt_base_ss0p72v125c.db"
+set link_library "/home1/local/SAED14nm_EDK_08_2024/SAED14nm_EDK_STD_RVT/liberty/nldm/base/saed14rvt_base_ss0p72v125c.db" 
 
 ####### Load Design and Elaborate #######
 ####### Update this portion to include the SoC RTL files ######
 ####### Make sure that the synthesized design is riscv_soc ####
 #analyze -format verilog -autoread ../core/core/rv32imsu/ 
 analyze -format verilog { 
-../core/rv32imsu/riscv_decode.v
-../core/rv32imsu/riscv_lsu.v
-../core/rv32imsu/riscv_regfile.v
-../core/rv32imsu/riscv_defs.v
-../core/rv32imsu/riscv_muldiv.v
-../core/rv32imsu/riscv_alu.v
-../core/rv32imsu/riscv_csr.v
-../core/rv32imsu/riscv_mmu_arb.v
-../core/rv32imsu/riscv_core.v
-../core/rv32imsu/riscv_mmu.v
-../core/rv32imsu/riscv_fetch.v
-../core/rv32imsu/riscv_exec.v
+../riscv_soc_master/core/core/rv32imsu/riscv_decode.v
+../riscv_soc_master/core/core/rv32imsu/riscv_lsu.v
+../riscv_soc_master/core/core/rv32imsu/riscv_regfile.v
+../riscv_soc_master/core/core/rv32imsu/riscv_defs.v
+../riscv_soc_master/core/core/rv32imsu/riscv_muldiv.v
+../riscv_soc_master/core/core/rv32imsu/riscv_alu.v
+../riscv_soc_master/core/core/rv32imsu/riscv_csr.v
+../riscv_soc_master/core/core/rv32imsu/riscv_mmu_arb.v
+../riscv_soc_master/core/core/rv32imsu/riscv_core.v
+../riscv_soc_master/core/core/rv32imsu/riscv_mmu.v
+../riscv_soc_master/core/core/rv32imsu/riscv_fetch.v
+../riscv_soc_master/core/core/rv32imsu/riscv_exec.v
+../riscv_soc_master/soc/axi4_arb.v
+../riscv_soc_master/soc/axi4lite_axi4_conv.v
+../riscv_soc_master/soc/axi4_lite_tap.v
+../riscv_soc_master/soc/axi4_retime.v
+../riscv_soc_master/soc/dport_bridge.v
+../riscv_soc_master/soc/gpio_defs.v
+../riscv_soc_master/soc/gpio.v
+../riscv_soc_master/soc/icache_data_ram.v
+../riscv_soc_master/soc/icache_tag_ram.v
+../riscv_soc_master/soc/icache.v
+../riscv_soc_master/soc/irq_ctrl_defs.v
+../riscv_soc_master/soc/irq_ctrl.v
+../riscv_soc_master/soc/riscv_soc.v
+../riscv_soc_master/soc/riscv_top.v
+../riscv_soc_master/soc/soc.v
+../riscv_soc_master/soc/spi_lite_defs.v
+../riscv_soc_master/soc/spi_lite.v
+../riscv_soc_master/soc/timer_defs.v
+../riscv_soc_master/soc/timer.v
+../riscv_soc_master/soc/uart_lite_defs.v
+../riscv_soc_master/soc/uart_lite.v
 }
 set top $module
 elaborate -lib work $top
